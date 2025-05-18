7e585c7778f156020ae28bdd05ad78d5c7641e53
RandomYaps <156994100+randomyaps@users.noreply.github.com>
2025-05-18 20:37:01 +0200

    It's the first blade file!
    This took an unnecessary amount of time to make.
    Finally we have a main page.

---
+++ resources/views/home.blade.php
@@ -0,0 +1,70 @@
+@extends('layouts.app')
+
+@section('content')
+<style>
+    .nav-scroller.mb-3, .navbar.mb-3 {
+        margin-bottom: 0rem !important;
+    }
+
+    .alert-padding {
+        display: none;
+    }
+
+    .alert {
+        margin-bottom: 0;
+    }
+
+    .home-main-content {
+        background: url('/imgs/xmas_small.jpg') no-repeat center center, linear-gradient(to bottom right, #312a6c, #852d91);
+        background-size: cover;
+        height: 300px;
+        color: #ececec;
+        display: flex;
+    }
+
+    .home-main-content > .container {
+        display: flex;
+        flex-direction: column;
+        align-self: center;
+    }
+</style>
+
+<div class="home-main-content">
+    <div class="container d-flex justify-content-center">
+        @if($user)
+            <h1 class="mb-4 home-text">Hi there, {{ htmlspecialchars($user->name) }}</h1>
+            <p><a href="{{ url('/places') }}" class="btn btn-success btn-lg mt-3 mr-2">join a game ></a></p>
+        @else
+            <h1 class="mb-4 home-text">We do what {{ $ProjectName }} does</h1>
+            <p class="home-text">{{ $ProjectName }} puts games on your not-phone.</p>
+            <p><a href="{{ url('/register') }}" class="btn btn-primary btn-lg mt-3 mr-2">Get started &rsaquo;</a></p>
+        @endif
+    </div>
+</div>
+
+<div class="container mt-4">
+    <div class="row">
+        <div class="col-sm-6 mb-2">
+            <a href="{{ $DiscordJoin }}" class="catalog-card">
+                <div class="card">
+                    <div class="card-body">
+                        <h3><img style="max-width:30px;max-height:30px" class="img-inline align-middle mr-1" src="{{ asset('imgs/discordsvg.svg') }}"> Discord</h3>
+                        <p class="text-muted mb-0">Join our Discord server and talk to people in our community!</p>
+                    </div>
+                </div>
+            </a>
+        </div>
+
+        <div class="col-sm-6 mb-2">
+            <a href="{{ $Twitter }}" class="catalog-card">
+                <div class="card">
+                    <div class="card-body">
+                        <h3><img style="max-width:30px;max-height:30px" class="img-inline align-middle mr-1" src="{{ asset('imgs/twittersvg.svg') }}"> Twitter</h3>
+                        <p class="text-muted mb-0">We tweet whenever we post a video, and may even hint at some upcoming events on our Twitter.</p>
+                    </div>
+                </div>
+            </a>
+        </div>
+    </div>
+</div>
+@endsection
