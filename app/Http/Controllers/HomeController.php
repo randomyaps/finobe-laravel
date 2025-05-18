be903c6312e6db53c33fce790181f57a4f5d816f
RandomYaps <156994100+randomyaps@users.noreply.github.com>
2025-05-18 20:32:33 +0200

    It's a HomeController!

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,10 +1,23 @@
 <?php
+// random 2025
+// Its a HomeController!
 
 namespace App\Http\Controllers;
-
-use Illuminate\Http\Request;
+ 
+use Illuminate\Support\Facades\Auth;
+use Illuminate\View\View;
 
 class HomeController extends Controller
 {
-    // todo, again
+    public function index(): View
+    {
+        // todo: put this in the .env if it's even possible
+        $user = Auth::user();
+        $ProjectName = config('app.name');
+        $baseUrl = url('/');
+        $DiscordJoin = 'https://discord.gg/placeholder';
+        $Twitter = 'https://twitter.com/placeholder';
+
+        return view('home', compact('user', 'ProjectName', 'baseUrl', 'DiscordJoin', 'Twitter'));
+    }
 }
